9f93955171e6ec0e1125b910f9fb8c24f7a60a30
Едуард Секан <eduardsekan@MacBook-Air.local>
2023-12-20 17:41:36 +0200

    end

--- src/index.js
+++ src/index.js
@@ -1,22 +1,14 @@
-import './styles/main.scss';
-import './swiper.js'
+import "./styles/main.scss"
+import "./swiper.js"
 
-// function highlightCurrentLink() {
-//   let url = window.location.href
-//   let links = document.querySelectorAll(".nav__link")
+document.addEventListener("DOMContentLoaded", function () {
+  var currentPath = window.location.pathname
+  var links = document.querySelectorAll(".nav__link")
 
-//   for (let i = 0; i < links.length; i++) {
-//     let link = links[i]
-//     let linkUrl = link.href
-
-//     if (url === linkUrl) {
-//       link.classList.add("current")
-//     } else {
-//       link.classList.remove("current")
-//     }
-//   }
-// }
-
-// document.addEventListener("DOMContentLoaded", function () {
-//   highlightCurrentLink()
-// })
+  links.forEach(function (link) {
+    var linkPath = link.getAttribute("href")
+    if (currentPath === linkPath) {
+      link.classList.add("current")
+    }
+  })
+})
